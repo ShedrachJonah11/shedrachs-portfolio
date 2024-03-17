7a0be67549fa47c81cd7dc144857d32b89800814
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-03-17 03:42:32 +0100

    edits

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -10,11 +10,12 @@ const TAB_DATA = [
     content: (
       <ul className="list-disc pl-2">
         <li>Node.js</li>
-        <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>Sequelize</li>
         <li>JavaScript</li>
+        <li>TypeScript</li>
         <li>React</li>
+        <li>React Native</li>
+        <li>Nextjs</li>
+        <li>PostgreSQL</li>
       </ul>
     ),
   },
@@ -23,8 +24,8 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
+        <li>AltSchool Africa</li>
+        <li>Federal Polytechnic Bauchi</li>
       </ul>
     ),
   },
@@ -33,8 +34,9 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
+        <li>GENESYS UPSKILL TRAINING PROGRAM (2023)</li>
+        <li>SOFTWARE ENGINEERING DIPLOMA</li>
+        <li>UNESCO YOUTH HACKATHON 2023</li>
       </ul>
     ),
   },
@@ -57,12 +59,14 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            As a dedicated software engineer, I thrive on crafting dynamic and
+            user-centric web applications. Proficient in JavaScript, React,
+            Redux, Node.js, Next.js, PostgreSQL, React Native, HTML, CSS, and
+            Git, I bring a versatile skill set to the table. Eager to learn and
+            adapt, I continually seek to broaden my expertise. A collaborative
+            team player at heart, I am driven by the opportunity to collaborate
+            with like-minded individuals to innovate and deliver exceptional
+            digital solutions.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -56,10 +56,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/ShedrachJonah11">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/shedrach-jonah-4894a722a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
